6da50a96cca47ab682687f2fbdec4bf7a020ff06
franclobo <fjbl2788@gmail.com>
2022-04-11 22:33:35 -0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,6 +3,6 @@ const closeBtn = document.querySelector('.close-menu');
 const optMenu = document.querySelector('.mobile-menu-navigation');
 const dropDown = document.querySelector('.dropdown-menu');
 
-mobileMenu.addEventListener('click', ()=>{
+mobileMenu.addEventListener('click', () => {
     dropDown.classList.remove('.hide-menu');
-})
+});
